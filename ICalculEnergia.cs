08e68c14457033c8653701b11e8fe97598bcec5b
Unai Calvet <unai.calvet.7e8@itb.cat>
2025-01-23 14:00:41 +0100

    classe Sistema Solar

--- ICalculEnergia.cs
+++ ICalculEnergia.cs
@@ -2,8 +2,22 @@
 
 namespace EcoEnergySolutions {
     interface ICalculEnergia {
+        /// <summary>
+        /// Configura el paràmetre de càlcul de l'energia
+        /// </summary>
+        /// <param name="valorParametre"></param>
         public void ConfigurarParametre(int valorParametre);
-        public void CalcularEnergia();
+
+        /// <summary>
+        /// Calcula la energia generada a partir de paràmetre
+        /// </summary>
+        /// <param name="valorParametre"></param>
+        /// <returns>Resultat del calcul de l'energia generada.</returns>
+        public float CalcularEnergia(int valorParametre);
+
+        /// <summary>
+        /// Mostra les propietats en format de taula
+        /// </summary>
         public void MostrarInforme();
     }
 }
---
+++ SistemaSolar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EcoEnergySolutions {
+    public class SistemaSolar : ASistemaEnergia {
+        private string defaultStringValue = "Solar";
+        public int HoresSol {  get; set; }
+
+        SistemaSolar(int horesSol, float energiaGenerada, string tipusSistema, string data) { 
+            HoresSol = horesSol;
+            EnergiaGenerada = energiaGenerada;
+            TipusSistema = tipusSistema;
+            Data = data;
+        }
+        SistemaSolar(int horesSol) : this(horesSol, 0, "solar", DateTime.Today.ToString("d")) { }
+        SistemaSolar() : this(0, 0, "solar", DateTime.Today.ToString("d")) { }
+
+        public override void ConfigurarParametre(int horesSol) { HoresSol = horesSol; }
+        public override float CalcularEnergia(int horesSol) { return horesSol * 1.5f;}
+        public override void MostrarInforme() {
+            Console.WriteLine("");
+        }
+    }
+}
